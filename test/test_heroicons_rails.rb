0e3501223ab6d96643d772f1ed44b9561ca6a327
narciero <narciero@me.com>
2023-08-10 13:04:24 -0600

    add tests

--- test/test_heroicons_rails.rb
+++ test/test_heroicons_rails.rb
@@ -5,15 +5,33 @@ require "test_helper"
 class TestHeroiconsRails < Minitest::Test
   include HeroiconsRails::Helper
 
+  def setup
+    @icon_name = "arrow-right"
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::HeroiconsRails::VERSION
   end
 
-  def test_it_renders_svg
-    icon = heroicon("arrow-right")
-
-    pp icon
-
+  def test_it_renders_icon
+    icon = heroicon(@icon_name)
     refute_nil icon
+  end
+
+  def test_it_renders_icon_with_variant
+    %i[mini solid outline].each do |variant|
+      icon = heroicon(@icon_name, variant: variant)
+      refute_nil icon
+    end
+  end
+
+  def test_it_renders_nothing_if_icon_name_is_invalid
+    icon = heroicon("non-existent-icon")
+    assert_nil icon
+  end
+
+  def test_it_renders_nothing_if_icon_variant_is_invalid
+    icon = heroicon(@icon_name, variant: :non_existent_variant)
+    assert_nil icon
   end
 end
